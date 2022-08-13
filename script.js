bcbf09d713f876293c0d4a19e649d86a6012f77b
Wellington <Wellingtoncpfilho@gmail.com>
2022-08-13 15:30:23 -0300

    JS entrega 1

---
+++ script.js
@@ -0,0 +1,8 @@
+let nome = "Nina";
+let sobrenome = "Hunter";
+let anoNascimento = 2020;
+let anoAtual = 2022;
+let idade = anoAtual- anoNascimento;
+let cidade = "São Gonçalo"
+
+console.log(`Eu sou a ${nome} ${sobrenome} tenho ${idade} anos e moro em ${cidade}`);
